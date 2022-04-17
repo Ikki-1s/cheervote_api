f1e630dc8d5f4229641cff666c15195bf0ed5819
Ikki-1s <ikki.engineer@gmail.com>
2022-04-17 14:29:07 +0900

    RuboCopの導入

--- app/controllers/api/v1/hr_members_controller.rb
+++ app/controllers/api/v1/hr_members_controller.rb
@@ -5,8 +5,8 @@ class Api::V1::HrMembersController < ApplicationController
   def index_of_prefecture
     # 都道府県ごとの小選挙区選出衆議院議員
     latest_hr_election_time_id = HrElectionTime.last.id
-    hr_members = HrMember.latest_of_prefecture(prefecture_id: params[:id], hr_election_time_id: latest_hr_election_time_id)
-    render :json => hr_members
+    hr_members = HrMember.latest_of_prefecture(prefecture_id: params[:id],
+                                               hr_election_time_id: latest_hr_election_time_id)
+    render json: hr_members
   end
-
 end
