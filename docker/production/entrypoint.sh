967f57d1e1423d2a91f2e85d9e06dec52cc8f0ae
Ikki-1s <ikki.engineer@gmail.com>
2023-06-14 16:12:22 +0900

    💚 entrypoint.shの修正

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -7,7 +7,7 @@ rm -f /app/tmp/pids/server.pid
 # WARNING: createとseedはfargateの初回のみ実行
 # WARNING: タスクを個々に作って実行のほうがいいかも
 # bundle exec bin/rails db:create
-# bundle exec bin/rails db:migrate
+bundle exec bin/rails db:migrate
 # bundle exec bin/rails db:seed_fu
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
