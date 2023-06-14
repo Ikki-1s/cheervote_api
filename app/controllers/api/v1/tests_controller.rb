c6e3b869f8ceabe0e809d4f878c4d29e58f99188
Ikki-1s <ikki.engineer@gmail.com>
2023-06-14 10:59:51 +0900

    🐛 database.ymlの修正

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::TestsController < ApplicationController
   # before_action :authenticate_api_v1_user!
 
   def index
-    render json: { message: 'hello' }
+    render json: { message: 'hello, world' }
   end
 end
